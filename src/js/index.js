c80a3093badd45eaac7efb7c891ce9f29f6b176a
Mikhail <mihail_kotov_86@mail.ru>
2018-10-25 18:40:42 +0300

    Добавляет JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,35 @@
 "use strict";
-
 svg4everybody();
+var menuElem = document.getElementById("filter_menu");
+var titleElem = menuElem.querySelector(".filter_title");
+var buttons = document.querySelectorAll(".button--del");
+var formListData = document.querySelector(".form__list--data");
+var inputs = formListData.querySelectorAll('.form__input');
+
+function showHiddingElems(evt) {
+  if (evt.keyCode !== 9) {
+    this.previousSibling.style.display = 'block';
+  }
+
+  for (let i = 0; i < Array.from(inputs).length; i++) {
+    let input = Array.from(inputs)[i];
+    if (input.value === '') {
+      input.previousSibling.style.display = 'none';
+    }
+  }
+}
 
 
-var menuElem = document.getElementById('filter_menu');
-var titleElem = menuElem.querySelector('.filter_title');
+Array.from(inputs).forEach(function (e) {
+  return e.addEventListener("keydown", showHiddingElems);
+});
+
+function remove() {
+  this.closest(".car__card").remove()
+}
 
 titleElem.onclick = function () {
-  menuElem.classList.toggle('open');
-};
+  menuElem.classList.toggle("open")
+}, Array.from(buttons).forEach(function (e) {
+  return e.addEventListener("click", remove)
+});
